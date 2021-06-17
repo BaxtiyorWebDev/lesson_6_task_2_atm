cbe68c0c7f88777af5cb49ced0a89a4565925a09
BaxtiyorMuhammadaliyev <baxtiyor.08042000@gmail.com>
2021-06-17 12:38:04 +0500

    editedDetectAuthService

--- src/main/java/uz/pdp/online/lesson_6_task_2_atm/component/DetectAuthenticationService.java
+++ src/main/java/uz/pdp/online/lesson_6_task_2_atm/component/DetectAuthenticationService.java
@@ -7,45 +7,62 @@ import org.springframework.stereotype.Service;
 import uz.pdp.online.lesson_6_task_2_atm.entity.User;
 import uz.pdp.online.lesson_6_task_2_atm.entity.enums.RoleEnum;
 
+import java.util.Optional;
+
 @Service
 public class DetectAuthenticationService {
 
     public static final boolean detectAuthForDirectorOrEmployee() {
         boolean allowed = false;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User) authentication.getPrincipal();
-        for (GrantedAuthority authority : user.getAuthorities()) {
-            if (authority.getAuthority().equals(RoleEnum.DIRECTOR.name())||authority.getAuthority().equals(RoleEnum.EMPLOYEE.name())) {
-                allowed = true;
-                return allowed;
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !authentication.getPrincipal().equals("anonymousUser")) {
+            User user = (User) authentication.getPrincipal();
+            for (GrantedAuthority authority : user.getAuthorities()) {
+                if (authority.getAuthority().equals(RoleEnum.DIRECTOR.name()) || authority.getAuthority().equals(RoleEnum.EMPLOYEE.name())) {
+                    allowed = true;
+                    return allowed;
+                }
             }
+            return allowed;
         }
-        return allowed;
+        return false;
     }
 
     public static final boolean detectAuthForDirector() {
-        boolean allowed = false;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User) authentication.getPrincipal();
-        for (GrantedAuthority authority : user.getAuthorities()) {
-            if (authority.getAuthority().equals(RoleEnum.DIRECTOR.name())) {
-                allowed = true;
-                return allowed;
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !authentication.getPrincipal().equals("anonymousUser")) {
+            boolean allowed = false;
+            User user = (User) authentication.getPrincipal();
+            for (GrantedAuthority authority : user.getAuthorities()) {
+                if (authority.getAuthority().equals(RoleEnum.DIRECTOR.name())) {
+                    allowed = true;
+                    return allowed;
+                }
             }
+            return allowed;
         }
-        return allowed;
+        return false;
     }
 
     public static final boolean detectAuthForEmployee() {
-        boolean allowed = false;
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        User user = (User) authentication.getPrincipal();
-        for (GrantedAuthority authority : user.getAuthorities()) {
-            if (authority.getAuthority().equals(RoleEnum.EMPLOYEE.name())) {
-                allowed = true;
-                return allowed;
+        if (authentication != null
+                && authentication.isAuthenticated()
+                && !authentication.getPrincipal().equals("anonymousUser")) {
+            boolean allowed = false;
+            User user = (User) authentication.getPrincipal();
+            for (GrantedAuthority authority : user.getAuthorities()) {
+                if (authority.getAuthority().equals(RoleEnum.EMPLOYEE.name())) {
+                    allowed = true;
+                    return allowed;
+                }
             }
+            return allowed;
         }
-        return allowed;
+        return false;
     }
 }
